852942b55b9310c26cc202ac318ef6eda36aa644
pradeep <pradeep>
2024-12-03 16:59:41 +0530

    Initial commit

---
+++ library_management/urls.py
@@ -0,0 +1,9 @@
+# library_management/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('library.urls')),  # Correctly include the library app URLs
+]
